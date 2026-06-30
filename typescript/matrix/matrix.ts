caf401d6791f7d670de101351d04e97efc435874
Adrian <99520451+Vassiat@users.noreply.github.com>
2026-06-29 21:01:35 -0400

    feat: Matrix exercise improved without double for iteration

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -5,18 +5,16 @@ export class Matrix {
   constructor(str: string) {
     this.rowsArray = this.stringToRows(str);
 
-    for (let i = 0; this.rows.length > i; i++) {
-      const ret: number[] = [];
-
-      const numberColumns = this.rows.length
-      for (let x = 0; numberColumns > x; x++) {
-        ret.push(this.rows[x][i]);
-      }
-
-      if (ret.some(Number)) {
-        this.columnsArray.push(ret);
-      }
-    }
+    const numberColumns = this.rows.length;
+    this.columnsArray = Array.from({ length: numberColumns }, (_, i) => {
+        const ret: number[] = [];
+         
+        for (let x = 0; numberColumns > x; x++) {
+          ret.push(this.rows[x][i]);       
+        }
+             
+        return  ret;
+    }).filter(arr => arr.some(Number) && arr.length);
   }
 
   get rows(): number[][] {
@@ -35,3 +33,7 @@ export class Matrix {
       );
   }
 }
+
+const _ = new Matrix("1 2 3\n4 5 6 2\n7 8 9\n8 7 6\n 2");
+
+console.log(_);
